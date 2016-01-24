e535a556c299f6e467f49c08f520466457c93c7d
Maxime Thoonsen <maximet@theodo.fr>
2016-01-24 17:38:25 +0100

    add home page

---
+++ src/AppBundle/Controller/HomeController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * Class DefaultController.
+ */
+class HomeController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function indexAction()
+    {
+        return $this->render('home/index.html.twig');
+    }
+}
